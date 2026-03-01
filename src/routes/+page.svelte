f18782839293275213ebb9108e57ed166bf22441
UltravioletVoodoo <jonbezeau@gmail.com>
2026-03-01 15:55:26 -0800

    Implement basic rotation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,28 @@
 <script lang="ts">
 	import Polyhedra from '$lib/components/Polyhedra.svelte';
+
+	let mouseXRotate = '0deg';
+	let mouseYRotate = '0deg';
+
+	function degreeify(v: number, totalV: number) {
+		const center = totalV / 2;
+		const distanceFromCenter = center - v;
+		const ratioToEdge = distanceFromCenter / center;
+		const deg = ratioToEdge * 50;
+
+		return `${deg}deg`;
+	}
+
+	function handleMouseMove(e: MouseEvent) {
+		mouseXRotate = degreeify(e.x, window.innerWidth);
+		mouseYRotate = degreeify(e.y, window.innerHeight);
+		console.log(`Position: (${e.x}, ${e.y})`);
+	}
 </script>
 
-<div class="container">
+<svelte:window onmousemove={handleMouseMove} />
+
+<div class="container" style="--mouseXRotate: {mouseXRotate}; --mouseYRotate: {mouseYRotate}">
 	<div class="header">header</div>
 	<div class="body">
 		<Polyhedra />
@@ -20,6 +40,9 @@
 		flex-direction: column;
 		align-items: center;
 		justify-content: space-between;
+		transform-style: preserve-3d;
+		transform: rotateY(var(--mouseXRotate)) rotateX(var(--mouseYRotate));
+		border: 1px solid green;
 	}
 	.header {
 	}
